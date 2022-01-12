9c1dc0e38dfe2a2528af46f93695dfe44067e149
0x6f736f646f <blackd0t@protonmail.com>
2022-01-12 18:37:54 +0300

    Lint with flake8
    Signed-off-by: 0x6f736f646f <blackd0t@protonmail.com>

--- lib/things.py
+++ lib/things.py
@@ -11,10 +11,12 @@ class Things:
     def create(self, thing, token):
         '''Creates thing entity in the database'''
         mf_resp = response.Response()
-        http_resp = requests.post(self.url + "/things", json=thing, headers={"Authorization": token})
+        http_resp = requests.post(
+            self.url + "/things", json=thing, headers={"Authorization": token})
         if http_resp.status_code != 201:
             mf_resp.error.status = 1
-            mf_resp.error.message = errors.handle_error(errors.things["create"], http_resp.status_code)
+            mf_resp.error.message = errors.handle_error(
+                errors.things["create"], http_resp.status_code)
         else:
             location = http_resp.headers.get("location")
             mf_resp.value = location.split('/')[2]
@@ -23,10 +25,15 @@ class Things:
     def create_bulk(self, things, token):
         '''Creates multiple things in a bulk'''
         mf_resp = response.Response()
-        http_resp = requests.post(self.url + "/things/bulk", json=things, headers={"Authorization": token})
+        http_resp = requests.post(
+            self.url + "/things/bulk",
+            json=things,
+            headers={"Authorization": token}
+        )
         if http_resp.status_code != 201:
             mf_resp.error.status = 1
-            mf_resp.error.message = errors.handle_error(errors.things["create_bulk"], http_resp.status_code)
+            mf_resp.error.message = errors.handle_error(
+                errors.things["create_bulk"], http_resp.status_code)
         else:
             mf_resp.value = http_resp.json()
         return mf_resp
@@ -34,10 +41,12 @@ class Things:
     def get(self, thing_id, token):
         '''Gets a thing entity for a logged-in user'''
         mf_resp = response.Response()
-        http_resp = requests.get(self.url + "/things/" + thing_id, headers={"Authorization": token})
+        http_resp = requests.get(
+            self.url + "/things/" + thing_id, headers={"Authorization": token})
         if http_resp.status_code != 200:
             mf_resp.error.status = 1
-            mf_resp.error.message = errors.handle_error(errors.things["get"], http_resp.status_code)
+            mf_resp.error.message = errors.handle_error(
+                errors.things["get"], http_resp.status_code)
         else:
             mf_resp.value = http_resp.json()
         return mf_resp
@@ -60,7 +69,8 @@ class Things:
         http_resp = requests.get(url, headers={"Authorization": token})
         if http_resp.status_code != 200:
             mf_resp.error.status = 1
-            mf_resp.error.message = errors.handle_error(errors.things["get_all"], http_resp.status_code)
+            mf_resp.error.message = errors.handle_error(
+                errors.things["get_all"], http_resp.status_code)
         else:
             mf_resp.value = http_resp.json()
         return mf_resp
@@ -73,49 +83,66 @@ class Things:
         http_resp = requests.get(url, headers={"Authorization": token})
         if http_resp.status_code != 200:
             mf_resp.error.status = 1
-            mf_resp.error.message = errors.handle_error(errors.things["get_by_channel"], http_resp.status_code)
+            mf_resp.error.message = errors.handle_error(
+                errors.things["get_by_channel"], http_resp.status_code)
         else:
             mf_resp.value = http_resp.json()
         return mf_resp
 
     def update(self, thing_id, token, thing):
         '''Updates thing entity'''
-        http_resp = requests.put(self.url + "/things/" + thing_id, json=thing, headers={"Authorization": token})
+        http_resp = requests.put(
+            self.url + "/things/" + thing_id,
+            json=thing,
+            headers={"Authorization": token}
+        )
         mf_resp = response.Response()
         if http_resp.status_code != 200:
             mf_resp.error.status = 1
-            mf_resp.error.message = errors.handle_error(errors.things["update"], http_resp.status_code)
+            mf_resp.error.message = errors.handle_error(
+                errors.things["update"], http_resp.status_code)
         return mf_resp
 
     def delete(self, thing_id, token):
         '''Deletes a thing entity from database'''
-        http_resp = requests.delete(self.url + "/things/" + thing_id, headers={"Authorization": token})
+        http_resp = requests.delete(
+            self.url + "/things/" + thing_id, headers={"Authorization": token})
         mf_resp = response.Response()
         if http_resp.status_code != 204:
             mf_resp.error.status = 1
-            mf_resp.error.message = errors.handle_error(errors.things["delete"], http_resp.status_code)
+            mf_resp.error.message = errors.handle_error(
+                errors.things["delete"], http_resp.status_code)
         return mf_resp
 
     def connect(self, channel_ids, thing_ids, token):
         '''Connects thing and channel'''
         payload = {
-          "channel_ids": channel_ids,
-          "thing_ids": thing_ids,
+            "channel_ids": channel_ids,
+            "thing_ids": thing_ids,
         }
-        http_resp = requests.post(self.url + "/connect", headers={"Authorization": token}, json=payload)
+        http_resp = requests.post(
+            self.url + "/connect",
+            headers={"Authorization": token},
+            json=payload
+        )
         mf_resp = response.Response()
         if http_resp.status_code != 201:
             mf_resp.error.status = 1
-            mf_resp.error.message = errors.handle_error(errors.things["connect"], http_resp.status_code)
+            mf_resp.error.message = errors.handle_error(
+                errors.things["connect"], http_resp.status_code)
         else:
             mf_resp.value = http_resp.json()
         return mf_resp
 
     def disconnect(self, channel_ids, thing_ids, token):
         '''Disconnect thing and channel'''
-        http_resp = requests.delete(self.url + "/channels/" + channel_ids + "/things/" + thing_ids, headers={"Authorization": token})
+        http_resp = requests.delete(self.url + "/channels/" + channel_ids +
+                                    "/things/" + thing_ids,
+                                    headers={"Authorization": token}
+                                    )
         mf_resp = response.Response()
         if http_resp.status_code != 204:
             mf_resp.error.status = 1
-            mf_resp.error.message = errors.handle_error(errors.things["disconnect"], http_resp.status_code)
+            mf_resp.error.message = errors.handle_error(
+                errors.things["disconnect"], http_resp.status_code)
         return mf_resp
